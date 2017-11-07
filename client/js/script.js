7a84b0fbd566e7cb610b0fbc82c8710ae93a6721
Xiaoqi(Sandy) Zhou <xiaomaoqiqi@gmail.com>
2017-11-07 02:04:47 +0000

    new stuff

---
+++ client/js/script.js
@@ -0,0 +1,15 @@
+var number = 5;
+console.log(number>5);
+console.log(number<5);
+if (number >5){
+    
+    console.log("number is greater than 5");
+    
+} else if (number<5)
+{
+    console.log("number is less than 5");
+}
+else {
+    
+    console.log("number is 5");
+}
