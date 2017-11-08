8bcb99e023b0b3b26200fb93d8521e938af24a6e
Xiaoqi(Sandy) Zhou <xiaomaoqiqi@gmail.com>
2017-11-08 14:13:02 +0000

    new stuff

---
+++ introEvening/testing/test.js
@@ -0,0 +1,25 @@
+var str = "CareerDevs";
+// console.log("contains of str");
+// console.log (str);
+// var number = 2017;
+
+// // console.log (str + " " + number);
+// // console.log (str, number);
+// if (str == "CareerDevs"){
+    
+    
+// }
+// console.log(str.split());
+
+
+// console.log(str.split("r"));
+// console.log(str.split(""));
+
+// str.split (",");
+
+console.log(str.split(":"));
+
+var array = ['a', 'b' ,'c', 'd', 'e']
+
+
+
